a040ea2d93f23ed7f437b7e69ee3a8dac7a3d50f
Abhishek Arun <abhishekarun021@gmail.com>
2023-03-12 16:43:24 +0530

    Insertion

--- AdmReg.aspx.cs
+++ AdmReg.aspx.cs
@@ -9,9 +9,37 @@ namespace Project1
 {
     public partial class AdmReg : System.Web.UI.Page
     {
+        ConnectionClass obj = new ConnectionClass();
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string mid = "select max(Reg_Id) from LogTab";
+            string reg_id = obj.Fn_sc(mid);
+
+            int regid = 0;
+            if (reg_id == "")
+            {
+                regid = 1;
+            }
+            else
+            {
+                int newreg_id = Convert.ToInt32(reg_id);
+                regid = newreg_id + 1;
+            }
+
+            string ins = "insert into AdminTab values(" + regid + ",'" + TextBox5.Text + "','" + TextBox6.Text + "')";
+            int i = obj.Fn_nq(ins);
+            string ins1 = "insert into LogTab values(" + regid + ",'" + TextBox7.Text + "','" + TextBox8.Text + "','admin','active')";
+            int j = obj.Fn_nq(ins1);
+
+            if (j != 0)
+            {
+                Label1.Text = "Successfully Registered";
+            }
         }
     }
 }
--- UserReg.aspx.cs
+++ UserReg.aspx.cs
@@ -9,9 +9,32 @@ namespace Project1
 {
     public partial class UserReg : System.Web.UI.Page
     {
+        ConnectionClass obj = new ConnectionClass();
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string maxid = "select max(Reg_Id) from LogTab";
+            string reg_id = obj.Fn_sc(maxid);
+
+            int regid = 0;
+            if (reg_id == "")
+            {
+                regid = 1;
+            }
+            else
+            {
+                int newrid = Convert.ToInt32(reg_id);
+                regid = newrid + 1;
+            }
+
+            string ins = "insert into UserTab values(" + regid + ",'" + TextBox1.Text + "'," + TextBox2.Text + ",'" + TextBox3.Text + "','" + TextBox4.Text + "')";
+            int i = obj.Fn_nq(ins);
+            string ins1 = "insert into LogTab values(" + regid + ",'" + TextBox5.Text + "','" + TextBox6.Text + "','User','Active')";
+            int j = obj.Fn_nq(ins1);
         }
     }
 }
